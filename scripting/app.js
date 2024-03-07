a7d63692894bc3ad4a3fffb472b1108e926345c3
Noah  Stortenbeker <100546@b5-450-53.ict.lab.locals>
2024-03-07 11:08:00 +0100

    first commit

---
+++ scripting/app.js
@@ -0,0 +1,16 @@
+function redirectToQuizIT() {
+    // Replace 'YOUR_WEBSITE_URL' with the actual URL you want to redirect to
+    window.location.href = '/quiz/quiz-italian.html';
+  }
+  function redirectToQuizGER() {
+    // Replace 'YOUR_WEBSITE_URL' with the actual URL you want to redirect to
+    window.location.href = '/quiz/quiz-german.html';
+  }
+  function redirectToQuizUSA() {
+    // Replace 'YOUR_WEBSITE_URL' with the actual URL you want to redirect to
+    window.location.href = '/quiz/quiz-american.html';
+  }
+
+  function redirectToInfo() {
+    window.location.href = '/contact/contact.html';
+  }
